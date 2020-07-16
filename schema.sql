95c1a64b4910f849049b3dec01e223b459ede636
willhitch <hitch.will@gmail.com>
2020-07-16 14:35:35 -0600

    fixed null on manager id input

--- schema.sql
+++ schema.sql
@@ -5,26 +5,23 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR (30),
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
+    manager_id INT UNSIGNED,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO department(name) VAlUES ("Finance");
+INSERT INTO role (title, salary, department_id) VAlUES ("Accountant",65000 , 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VAlUES ("Will", "Hitch", 1, null);
